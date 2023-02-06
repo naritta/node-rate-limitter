369066457741f16b6dc841cb0f693d4a1d6d718f
naritta <narittan@gmail.com>
2023-02-06 23:51:11 +0900

    Add authentication by jsonwebtoken

--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,9 @@ WORKDIR /usr/src/app
 
 COPY package*.json ./
 
-RUN npm install
+RUN npm install --save express body-parser jsonwebtoken
 
 COPY . .
 
-EXPOSE 8080
+EXPOSE 3000
 CMD [ "node", "server.js" ]
--- server.js
+++ server.js
@@ -1,25 +1,35 @@
-'use strict';
-
 const express = require('express');
-
-// Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
-
-// App
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const config = require('./config');
+const authenticate = require('./authenticate');
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World');
+
+app.use(bodyParser.json());
+
+app.post('/login', (req, res) => {
+  const payload = {
+    email: req.body.email
+  };
+
+  const token = jwt.sign(payload, config.jwt.secret, config.jwt.options);
+
+  const body = {
+    email: req.body.email,
+    token: token,
+  };
+  res.status(200).json(body);
 });
 
-app.get('/ip', (req, res) => {
+app.get('/private', authenticate, (req, res) => {
+  res.status(200).json({
+    message: 'Hello!',
+    authEmail: req.jwtPayload.email,
+  });
+});
+
+app.get('/public', (req, res) => {
   res.send('Hello ip');
 });
 
-app.get('/token', (req, res) => {
-  res.send('Hello token');
-});
-
-app.listen(PORT, HOST, () => {
-  console.log(`Running on http://${HOST}:${PORT}`);
-});
+app.listen(3000, () => console.log('Listening on port 3000...'));
